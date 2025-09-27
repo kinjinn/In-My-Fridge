db5494517dc6c74af46c4445dec197dd3f0b445e
Thomas-Ishida <ishida.thomas@hotmail.com>
2025-09-27 18:38:22 -0400

    recipe generator working

---
+++ App_swift/Recipe_App/Recipe.swift
@@ -0,0 +1,15 @@
+// Recipe.swift
+import Foundation
+
+struct Recipe: Codable, Identifiable {
+    let id = UUID() // Add a unique ID for SwiftUI
+    let title: String
+    let description: String
+    let ingredients_used: [String]
+    let instructions: [String]
+
+    // We need this because 'id' is not in the JSON from the API
+    private enum CodingKeys: String, CodingKey {
+        case title, description, ingredients_used, instructions
+    }
+}
